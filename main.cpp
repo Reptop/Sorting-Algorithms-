a96066988e886c4643c3cc6c4430650c697846f7
Ren <36342803+Reptop@users.noreply.github.com>
2021-10-01 12:55:03 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <bits/stdc++.h>
+#include <fstream> 
 using namespace std;
 
 // A utility function to swap two elements
@@ -70,28 +71,114 @@ void Bubble(int arr[], int size) {
     } 
   }
 }
+// Merges two subarrays of array[].
+// First subarray is arr[begin..mid]
+// Second subarray is arr[mid+1..end]
+void merge(int array[], int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+  
+    // Create temp arrays
+    auto *leftArray = new int[subArrayOne],
+         *rightArray = new int[subArrayTwo];
+  
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+  
+    auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+        indexOfSubArrayTwo = 0; // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+  
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+  
+// begin is for left index and end is
+// right index of the sub-array
+// of arr to be sorted */
+void mergeSort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return; // Returns recursively
+  
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
 
+void printmerge(int A[], int size) {
+    for (auto i = 0; i < size; i++)
+        cout << A[i] << " ";
+}
+  
 
-
+int testy[10000];
 
 int main() {
-int input; 
-cout << "1. bubble 2. quick" << endl; 
+ifstream file("dataset.txt");
+if(file.is_open()) {
+        for(int i = 0; i < 10000; ++i) {
+            file >> testy[i];
+        }
+    }
+
+int input;
+cout << "1. bubble 2. quick 3. mergesort" << endl; 
 cin >> input; 
-int test[] = {14, 32, 25, 5, 4, 23, 43, 5, 34};
-int length = sizeof(test)/sizeof(test[0]);  
+//int test[] = {14, 32, 25, 5, 4, 23, 43, 5, 34};
+//int length = sizeof(test)/sizeof(test[0]);  
+int len = 10000; 
 switch(input) {
   case 1:
-  Bubble(test, length);
+  Bubble(testy, len);
   cout << "Sorted Array: " << endl;
-  printArray(test, length); 
+  printArray(testy, len); 
   break; 
   case 2: 
-  quickSort(test, 0, length-1);
+  quickSort(testy, 0, len-1);
   cout << "Sorted Array: " << endl;
-  printArray(test, length);
+  printArray(testy, len);
   break;  
-}
+  case 3: 
+  mergeSort(testy, 0, len-1); 
+  cout << "Sorted Array: " << endl;
+  printmerge(testy, len); 
+  break; 
+  }
+  if (input != 1 || input != 2 || input != 3) {
+    cout << "Invalid Input\n";
+    //cin >> input;
+  }
+
 //cout << "Invalid input" << endl;
 //main();  
 return 0; 
