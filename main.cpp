bfc09d063a4017dedbb0eba9d5ee14e7c333286b
Ren <36342803+Reptop@users.noreply.github.com>
2021-10-04 09:12:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
-
 #include <bits/stdc++.h>
 #include <fstream> 
 using namespace std;
 
 // A utility function to swap two elements
-void swap(int* a, int* b)
-{
+void swap(int* a, int* b) {
 	int t = *a;
 	*a = *b;
 	*b = t;
@@ -16,16 +14,13 @@ the pivot element at its correct position in sorted
 array, and places all smaller (smaller than pivot)
 to left of pivot and all greater elements to right
 of pivot */
-int partition (int arr[], int low, int high)
-{
+int partition (int arr[], int low, int high) {
 	int pivot = arr[high]; // pivot
 	int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
-
-	for (int j = low; j <= high - 1; j++)
-	{
+	
+	for (int j = low; j <= high - 1; j++) {
 		// If current element is smaller than the pivot
-		if (arr[j] < pivot)
-		{
+		if (arr[j] < pivot) {
 			i++; // increment index of smaller element
 			swap(&arr[i], &arr[j]);
 		}
@@ -38,10 +33,8 @@ int partition (int arr[], int low, int high)
 arr[] --> Array to be sorted,
 low --> Starting index,
 high --> Ending index */
-void quickSort(int arr[], int low, int high)
-{
-	if (low < high)
-	{
+void quickSort(int arr[], int low, int high) {
+	if (low < high) {
 		/* pi is partitioning index, arr[p] is now
 		at right place */
 		int pi = partition(arr, low, high);
